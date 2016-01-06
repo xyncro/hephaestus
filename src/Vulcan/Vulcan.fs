c1d8af09c623d6d31cf9df1d86ca62f8d3b340f1
kolektiv <andrew@xyncro.com>
2016-01-06 22:34:41 +0000

    beginnings of a vaguely usable model

--- src/Vulcan/Vulcan.fs
+++ src/Vulcan/Vulcan.fs
@@ -1,14 +1,28 @@
 ﻿module Vulcan
 
+open System
 open Aether
 open Hekate
 
-(* Functions *)
+(* Notes
+
+   Type parameter names are used consistently:
+   - 'c for the type of Configuration
+   - 's for the type of State *)
+
+(* Types
+
+   Core Vulcan types of various classifications, representing the common
+   building blocks of Vulcan machinery which will regularly be exposed to
+   users of the Vulcan system (i.e. those designing and implementing
+   Vulcan components. *)
+
+(* Core *)
 
 type Vulcan<'a,'s> =
     's -> Async<'a * 's>
 
-(* Types *)
+(* Decisions *)
 
 type VulcanDecision<'s> =
     | Function of Vulcan<VulcanDecisionValue,'s>
@@ -26,92 +40,197 @@ type VulcanDecision<'s> =
     | Right
     | Left
 
-(* Common *)
+type VulcanDecisionConfigurator<'c,'s> =
+    'c -> VulcanDecision<'s>
 
-module Common =
+(* Components
 
-    type Edge =
-        | Empty
-        | Direction of VulcanDecisionValue
+   The Vulcan Component model, a highly restricted abstraction of the
+   conceptual decision graph, allowing only binary (Left|Right) Decisions or
+   Terminals to exist as a Component of a graph (where the left and right case
+   of a Decision is - recursively - a Component).
 
-    type Node<'s> =
-        | Nullary of Vulcan<unit,'s>
-        | Unary of Vulcan<unit,'s>
+   This allows for graphs to be
+   defined (including graphs with cycles if required through the use of rec
+   bindings) and defined as reusable elements where convenient. *)
 
-(* Configuration *)
-
-module Configuration =
+[<AutoOpen>]
+module Components =
 
     (* Types
 
-       Graph types for the two states of the configuration process:
-       - a configured graph which has had the configuration data applied
-         to relevant nodes, and
-       - an unconfigured graph, where decision nodes are not yet configured
-         and are functions which will take the configuration data to provide
-         a configured decision (which may already be final, or may need further
-         evaluation at runtime).
-         
-       Type parameters are used consistently:
-       - 'c for the type of Configuration
-       - 's for the type of State *)
+       Types for a component model of a decision graph, using two simple primitive
+       types from which any decision graph can be constructed and optimized,
+       etc. *)
 
-    (* Configured
+    type VulcanComponent<'c,'s> =
+        | Decision of VulcanComponentDecision<'c,'s>
+        | Terminal of VulcanComponentTerminal<'s>
 
-       A configured graph, parameterized solely by the state type which will
-       be threaded through the computation. *)
-
-    type ConfiguredGraph<'s> =
-        | Graph of Graph<string,ConfiguredNode<'s>,Common.Edge>
-
-        static member graph_ : Isomorphism<ConfiguredGraph<'s>,Graph<string,ConfiguredNode<'s>,Common.Edge>> =
-            (fun (Graph x) -> x), (Graph)
-
-     and ConfiguredNode<'s> =
-        | Node of Common.Node<'s>
-        | Decision of ConfiguredDecision<'s>
-
-        static member node_ : Epimorphism<ConfiguredNode<'s>,Common.Node<'s>> =
-            (function | Node n -> Some n
-                      | _ -> None), (Node)
-
-        static member decision_ : Epimorphism<ConfiguredNode<'s>,ConfiguredDecision<'s>> =
+        static member decision_ : Epimorphism<VulcanComponent<'c,'s>,VulcanComponentDecision<'c,'s>> =
             (function | Decision d -> Some d
                       | _ -> None), (Decision)
 
-     and ConfiguredDecision<'s> =
-        | Decision of VulcanDecision<'s>
+        static member terminal_ : Epimorphism<VulcanComponent<'c,'s>,VulcanComponentTerminal<'s>> =
+            (function | Terminal t -> Some t
+                      | _ -> None), (Terminal)
 
-        static member decision_ : Isomorphism<ConfiguredDecision<'s>,VulcanDecision<'s>> =
-            (fun (Decision d) -> d), (Decision)
+     and VulcanComponentDecision<'c,'s> =
+        string * VulcanDecisionConfigurator<'c,'s> * (VulcanComponent<'c,'s> * VulcanComponent<'c,'s>)
 
-    (* Unconfigured
+     and VulcanComponentTerminal<'s> =
+        string * Vulcan<unit,'s>
 
-       An unconfigured graph, parameterized by state type, and by the type
-       of the configuration data which will be passed to unconfigured
-       Decisions to effectively reify to a configured Decision. *)
+    (* Decisions
 
-    type UnconfiguredGraph<'c,'s> =
-        | Graph of Graph<string,UnconfiguredNode<'c,'s>,Common.Edge>
+       Helpful shorthand functions for creating decisions, including literal decisions
+       where the decision of Left or Right is known at compile time, and the unselected
+       decision is assigned a simple null terminal.
 
-        static member graph_ : Isomorphism<UnconfiguredGraph<'c,'s>,Graph<string,UnconfiguredNode<'c,'s>,Common.Edge>> =
-            (fun (Graph x) -> x), (Graph)
+       This can be useful for defining components which effectively act as a pipeline
+       and only one logical connection point is coherent for the end user of the
+       component. It is assumed that dead (unreachable) paths will be optimized out
+       of the final executable form of the resultant decision graph in general use. *)
 
-     and UnconfiguredNode<'c,'s> =
-        | Node of Common.Node<'s>
-        | Decision of UnconfiguredDecision<'c,'s>
+    [<RequireQualifiedAccess>]
+    module Decision =
 
-     and UnconfiguredDecision<'c,'s> =
-        | Configure of ('c -> ConfiguredDecision<'s>)
+        let private configure value =
+            fun _ -> Literal value
 
-    (* Configuration
+        let private terminal () =
+            Terminal (sprintf "terminal.%A" (Guid.NewGuid ()), fun s -> async { return (), s })
 
-       Functions for applying configuration data to an unconfigured graph, giving
-       a configured graph. *)
+        let decision name configure left right =
+            Decision (name, configure, (left, right))
 
-    let configure<'c,'s> configuration =
-            Optic.get (Lens.ofIsomorphism UnconfiguredGraph<'c,'s>.graph_)
-         >> Graph.Nodes.map (fun _ ->
-                function | Node n -> ConfiguredNode.Node n
-                         | Decision (Configure apply) -> ConfiguredNode.Decision (apply configuration))
-         >> ConfiguredGraph<'s>.Graph
+        let left name left =
+            Decision (name, configure Left, (left, terminal ()))
+
+        let right name right =
+            Decision (name, configure Right, (terminal (), right))
+
+    (* Terminals *)
+
+    [<RequireQualifiedAccess>]
+    module Terminal =
+
+        let terminal name f =
+            Terminal (name, f)
+
+(* Graphs
+
+   The Vulcan Graph implementation, turning the logical Vulcan Component representation
+   of a decision graph in to an optimized executable form (given appropriate
+   configuration data to configure a graph before optimization).
+   
+   The resultant graph effectively behaves as an Async State monad while being
+   transparent and modifiable - potentially supporting differing forms of optimization,
+   error handling etc. via mapping of the graph (including a possible plugin model
+   which would act as a "plugin-based graph compiler"). *)
+
+[<AutoOpen>]
+module Graphs =
+
+    (* Common *)
+
+    [<RequireQualifiedAccess>]
+    module Common =
+
+        type Node<'s> =
+            | Terminal of Vulcan<unit,'s>
+
+         and Edge =
+            | Empty
+            | Value of VulcanDecisionValue
+
+    (* Translation *)
+
+    [<RequireQualifiedAccess>]
+    module Translation =
+
+        (* Types
+
+           A translation graph, parameterized by state type, and by the type
+           of the configuration data which will be passed to translation
+           decisions to effectively reify to a Vulcan Decision.
+
+           Translation graphs are the result of the Translation translation from the Vulcan
+           Component model to a more general graph model. *)
+
+        type TranslationGraph<'c,'s> =
+            | Graph of TranslationGraphType<'c,'s>
+
+            static member graph_ : Isomorphism<TranslationGraph<'c,'s>,TranslationGraphType<'c,'s>> =
+                (fun (Graph x) -> x), (Graph)
+
+         and TranslationGraphType<'c,'s> =
+            Graph<string,TranslationNode<'c,'s>,Common.Edge>
+
+         and TranslationNode<'c,'s> =
+            | Node of Common.Node<'s>
+            | Configure of VulcanDecisionConfigurator<'c,'s>
+
+        (* Translation
+
+           Functions for translating a Vulcan Component to a graph based representation
+           of the equivalent (unmodified) decision graph, prior to applying instance specific
+           configuration to the graph. *)
+
+        let rec private nodes ns =
+            function | Decision (n, c, (l, r)) -> (n, Configure c) :: nodes [] l @ nodes [] r @ ns
+                     | Terminal (n, f) -> (n, Node (Common.Terminal f)) :: ns
+
+        let translate c =
+            Graph (Graph.create (nodes [] c) [])
+
+    (* Configuration *)
+
+    module Configuration =
+
+        (* Types
+
+           A configured graph, parameterized solely by the state type which will
+           be threaded through the computation. *)
+
+        type ConfiguredGraph<'s> =
+            | Graph of ConfiguredGraphType<'s>
+
+            static member graph_ : Isomorphism<ConfiguredGraph<'s>,ConfiguredGraphType<'s>> =
+                (fun (Graph x) -> x), (Graph)
+
+         and ConfiguredGraphType<'s> =
+            Graph<string,ConfiguredNode<'s>,Common.Edge>
+
+         and ConfiguredNode<'s> =
+            | Node of Common.Node<'s>
+            | Decision of VulcanDecision<'s>
+
+            static member node_ : Epimorphism<ConfiguredNode<'s>,Common.Node<'s>> =
+                (function | Node n -> Some n
+                          | _ -> None), (Node)
+
+            static member decision_ : Epimorphism<ConfiguredNode<'s>,VulcanDecision<'s>> =
+                (function | Decision d -> Some d
+                          | _ -> None), (Decision)
+
+        (* Configuration
+
+           Functions for applying configuration data to an unconfigured graph, giving
+           a configured graph. *)
+
+        let private (|Node|_|) =
+            function | Translation.TranslationNode.Node n -> Some n
+                     | _ -> None
+
+        let private (|Configure|_|) =
+            function | Translation.Configure c -> Some c
+                     | _ -> None
+
+        let configure<'c,'s> configuration =
+                Optic.get (Lens.ofIsomorphism Translation.TranslationGraph<'c,'s>.graph_)
+             >> Graph.Nodes.map (fun _ ->
+                    function | Node node -> Node node
+                             | Configure configure -> Decision (configure configuration)
+                             | _ -> failwith "")
+             >> ConfiguredGraph<'s>.Graph
--- tests/Vulcan.Console/Program.fs
+++ tests/Vulcan.Console/Program.fs
@@ -2,7 +2,49 @@
 
 open Vulcan
 
+(* Configuration *)
+
+type ExampleConfiguration =
+    { AllowEven: bool }
+
+(* State *)
+
+type ExampleState =
+    { Value: int }
+
+(* Terminals *)
+
+let printer name =
+    Terminal.terminal name (fun s ->
+        async {
+            printfn "%s Value: %i" name s.Value
+            return (), s })
+
+(* Decisions *)
+
+let filterEven =
+    Decision.decision "filter even" (fun configuration ->
+        match configuration.AllowEven with
+        | true ->
+            Literal Right
+        | _ ->
+            Function (fun state ->
+                async {
+                    return match state.Value with
+                           | value when value % 2 <> 0 -> Right, state
+                           | _ -> Left, state }))
+
+(* Composition *)
+
+let protocol =
+    filterEven (printer "not allowed") (printer "complete")
+
+(* Main *)
+
 [<EntryPoint>]
 let main _ =
+
+    let translated = Graphs.Translation.translate protocol
+    let configured = Graphs.Configuration.configure { AllowEven = false } translated
 
     0
